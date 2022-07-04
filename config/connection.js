1e92387e7892ca600ba33f1d4b6f8cebd89ad3be
sam-pettyjohn <sammpj47@gmail.com>
2022-07-03 21:36:21 -0400

    initialized connection.js

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: 3000,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_KEY,
+    database: process.env.MYSQL_DB
+});
+
+module.exports = connection;
